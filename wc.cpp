9700d4b94be6eec576eb5e532de946540dcaa7ec
unknown <>
2017-02-09 16:20:47 +0800

    mod wc

---
+++ wc.cpp
@@ -0,0 +1,42 @@
+#include "wc.h"
+
+const int buff_len = 2048;
+char buff[buff_len];
+
+int main()
+{
+	try
+	{
+		parser mparser(mlex_rules, mparse_rules, mexpr_rules);
+		while (1)
+		{
+			printf(">>> ");
+			auto* ptr = buff;
+			while (scanf("%[^\n]s", ptr) == 1)
+			{
+				fflush(stdin);
+				ptr += strlen(ptr);
+				*ptr++ = '\n';
+				*ptr = 0;
+			}
+			if (!*buff) break;
+			try
+			{
+				mparser.parse(buff);
+				//mparser.parse(tokens);
+			}
+			catch (const err& e)	// poly
+			{
+				e.alert();
+			}
+			fflush(stdin);
+			*buff = 0;
+		}
+		lModule->dump();
+		system("pause");
+	}
+	catch (const err& e)	// poly
+	{
+		e.alert();
+	}
+}
